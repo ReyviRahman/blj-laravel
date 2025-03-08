7b6e1a213a2cf8fbda9f60ff8cf071a4a2c63781
Reyvi Rahman <reyvirahman@gmail.com>
2025-03-08 20:14:29 +0700

    RedesignUI

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,57 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1 class="text-3xl font-bold mb-8">{{ $title }}</h1>
-  @foreach ($posts as $post)
-    <article class="mb-5 border-b-1 border-gray-900">
-      <div class="group relative">
-        <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 hover:text-gray-600">
-          <a href="/blog/{{ $post->slug }}">
-            <span class="absolute inset-0"></span>
-            {{ $post->title }}
-          </a>
-        </h3>
-        <p>By: <a href="/author/{{ $post->user->username }}" class="relative text-gray-900 hover:text-gray-600 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
-          after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="relative text-gray-900 hover:text-gray-600 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
-          after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full">{{ $post->category->name }}</a></p>
-        <p class="mt-2 line-clamp-3 text-sm/6">{{ $post->excerpt }}</p>
-      </div>
-    </article>
-  @endforeach
+  <h1 class="text-3xl font-bold mb-8 text-center">{{ $title }}</h1>
 
+  <form action="/blog">
+    <div class="col-span-full">
+      <div class="mt-2 flex gap-2">
+        <input type="text" name="search" id="search" autocomplete="search" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Search..." >
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">Search</button>
+      </div>
+      
+    </div>
+    
+  </form>
+  
+
+  @if ($posts->count())
+    <div class="bayang rounded mt-5">
+      <div class="flex flex-col ">
+        <img src="https://picsum.photos/500/300?grayscale" alt="..." class="max-h-100 rounded object-fill">
+      </div>
+      <div class="p-4 text-center"> 
+        <h1 class="text-2xl font-semibold"> <a href="/blog/{{ $posts[0]->slug }}">{{ $posts[0]->title }}</a></h1>
+        <p>By: <a href="/author/{{ $posts[0]->user->username }}" class="font-semibold relative text-gray-900 hover:text-gray-600 
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
+          after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full">{{ $posts[0]->user->name }}</a> in <a href="/category/{{ $posts[0]->category->slug }}" class="relative text-gray-900 hover:text-gray-600 
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
+          after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full font-semibold">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}</p>
+        <p>{{ $posts[0]->excerpt }}</p>
+      </div>
+    </div>
+  
+
+  <div class="grid grid-cols-3 gap-4 mt-5">
+    @foreach ($posts->skip(1) as $post)
+      <div class="bayang rounded">
+        <div class="flex flex-col ">
+          <img src="https://picsum.photos/500/300?grayscale" alt="..." class="max-h-100 rounded object-fill">
+        </div>
+        <div class="p-4 text-center"> 
+          <h1 class="text-2xl font-semibold"> <a href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h1>
+          <p>By: <a href="/author/{{ $post->user->username }}" class="font-semibold relative text-gray-900 hover:text-gray-600 
+            after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
+            after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="relative text-gray-900 hover:text-gray-600 
+            after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] 
+            after:bg-gray-600 after:transition-all after:duration-300 hover:after:w-full font-semibold">{{ $post->category->name }}</a> {{ $post->created_at->diffForHumans() }}</p>
+          <p>{{ $post->excerpt }}</p>
+        </div>
+      </div>
+    @endforeach
+  </div>
+
+  @else
+    <p class="text-center">No Post Found</p>
+  @endif
 @endsection
