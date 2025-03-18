847c1ded8f003432008935145fee26de9522e380
Reyvi Rahman <reyvirahman@gmail.com>
2025-03-18 09:09:51 +0700

    PreviewUpdateDeleteImage

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -70,16 +70,18 @@
           <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Post Image</label>
           <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 @error('image') !border-red-500 @enderror px-6 py-10">
             <div class="text-center">
-              <svg class="mx-auto size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
+              <img id="img-preview" src="" alt="preview" class="hidden max-h-50 rounded object-fill">
+              <svg id="placeholderImage" class="mx-auto size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
                 <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
               </svg>
-              <p class="inifilenya text-xs/5 text-gray-600 w-[190px]">PNG, JPG, GIF up to 10MB</p>
-              <div class="mt-4 flex text-sm/6 text-gray-600">
+
+              <p class="inifilenya text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+              <div class="mt-4 text-sm/6 text-gray-600">
                 <label for="image" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
                   <span>Upload a file</span>
                   <input id="image" name="image" type="file" class="sr-only">
                 </label>
-                <p class="pl-1">or drag and drop</p>
+                <span>or drag and drop</span>
               </div>
               <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
             </div>
@@ -130,8 +132,20 @@
   });
 
   fileInput.addEventListener("change", () => {
+    const imgPreview = document.getElementById('img-preview');
+
     if (fileInput.files.length > 0) {
       displayFileName(fileInput.files[0]);
+    }
+    document.getElementById('placeholderImage').style.display = 'none';
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(fileInput.files[0]);
+
+    oFReader.onload = (oFREvent) => {
+      imgPreview.src = oFREvent.target.result;
     }
   });
 
